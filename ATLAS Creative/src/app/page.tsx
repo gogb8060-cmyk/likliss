1319c810777317ba71844acd7ba56ce9bfba4831
gogb8060-cmyk <gogb8060@gmail.com>
2026-09-12 21:26:13 +0300

    Delete ATLAS Creative directory

--- ATLAS Creative/src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-// This page is intentionally empty - content is rendered from layout.tsx
-// The home page uses the default content from the layout
-
-export default function Home() {
-  return null;
-}
